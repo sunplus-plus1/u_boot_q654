2ce446af1a5767bf417336f47ed8d5c165f5dd53
Wells Lu <wells.lu@sunplus.com>
2024-01-26 14:36:30 +0800

    Modified MDTPARTS_DEFAULT based on new partitioning.

--- include/configs/pentagram_common_sp7350.h
+++ include/configs/pentagram_common_sp7350.h
@@ -102,9 +102,9 @@
 //#define CONFIG_MTD_DEVICE		/* needed for mtdparts cmd */
 #define MTDIDS_DEFAULT			"nand0=sp_spinand.0"
 #if 0 // Set default mtdparts for zebu sim
-#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),32512k(rootfs)"
+#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),4096k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),32512k(rootfs)"
 #else
-#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),229120k(rootfs)"
+#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),4096k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),-(rootfs)"
 #endif
 #ifdef CONFIG_SP_PARANAND
 #undef CONFIG_SYS_NAND_BASE
@@ -114,9 +114,9 @@
 #define MTDIDS_DEFAULT                  "nand0=sp_paranand.0"
 #if (NAND_PAGE_SIZE == 8)
 /* Samsung MLC/Mirco SLC 8k page */
-#define MTDPARTS_DEFAULT                "sp_paranand.0:1m(nand_header),1m(xboot1),1m(uboot1),1m(uboot2),2m(fip),1m(env),1m(env_redund),1m(dtb),25m(kernel),-(rootfs)"
+#define MTDPARTS_DEFAULT                "sp_paranand.0:1m(nand_header),1m(xboot1),2m(uboot1),2m(uboot2),2m(fip),1m(env),1m(env_redund),1m(dtb),25m(kernel),-(rootfs)"
 #else
-#define MTDPARTS_DEFAULT                "sp_paranand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),-(rootfs)"
+#define MTDPARTS_DEFAULT                "sp_paranand.0:128k(nand_header),384k(xboot1),1536k(uboot1),4096k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),-(rootfs)"
 #endif
 #endif
 #endif
