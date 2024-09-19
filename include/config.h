34f0bfa44954d419fa48e397e3e7066d5cae66f2
haiq.tang <haiq.tang@sunmedia.com.cn>
2024-09-19 14:23:19 +0800

    remove the files generated by compilation

--- include/config.h
+++
@@ -1,6 +0,0 @@
-/* Automatically generated - do not edit */
-#define CFG_BOARDDIR board/sunplus/sp7350
-#include <configs/pentagram_common_sp7350.h>
-#include <asm/config.h>
-#include <linux/kconfig.h>
-#include <config_fallbacks.h>
